86375552ca89dc76f2f3c82afbbfdcd7b91a18df
Felicia Ebikon <feliciaebikon@gmail.com>
2022-03-09 17:17:59 +0100

    fetch data from graphcms

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,15 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import { PostCard, Categories, PostWidget } from '../components';
+import { getPosts } from '../services';
 
-const posts = [
-  { title: 'React Testing', excerpt: 'Learn React Testing' },
-  { title: 'React with Tailwind', excerpt: 'Learn React with Tailwind' },
+// const posts = [
+//   { title: 'React Testing', excerpt: 'Learn React Testing' },
+//   { title: 'React with Tailwind', excerpt: 'Learn React with Tailwind' },
 
-];
+// ];
 
-const Home: NextPage = () => {
+const Home: NextPage = ({ posts }) => {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
@@ -17,7 +18,7 @@ const Home: NextPage = () => {
       </Head>
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className='lg:col-span-8 col-span-1'>
-          {posts.map((post) => <PostCard post={post} key={post.title} />)}
+          {posts.map((post) => <PostCard post={post.node} key={post.title} />)}
         </div>
         <div className='lg:col-span-4 col-span-1'>
           <div className='lg:sticky relative top-8'>
@@ -26,10 +27,16 @@ const Home: NextPage = () => {
           </div>
         </div>
       </div>
-
-      
     </div>        
   )      
 }
 
 export default Home
+
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+
+  return {
+    props: { posts },
+  }
+}
